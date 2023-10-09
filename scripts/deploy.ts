c55443172e11343ccd028b71282b635491c47248
Jim Toth <jim@artby.city>
2023-10-09 14:51:41 -0400

    WIP: Deploy local curation contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,8 +3,11 @@ import fs from 'fs/promises'
 import Arweave from 'arweave'
 import { JWKInterface } from 'arweave/node/lib/wallet'
 import minimist from 'minimist'
-import { defaultCacheOptions, Tags, Warp, WarpFactory } from 'warp-contracts'
+import { defaultCacheOptions, Tag, Transaction, Warp, WarpFactory } from 'warp-contracts'
+import { DeployPlugin } from 'warp-contracts-plugin-deploy'
 
+// const APP_NAME = process.env.APP_NAME || 'ArtByCity-Development'
+// const APP_VERSION = process.env.APP_VERSION || 'development'
 const arweaveConfig = {
   protocol: process.env.ARWEAVE_PROTOCOL || 'http',
   host: process.env.ARWEAVE_HOST || 'localhost',
@@ -21,45 +24,41 @@ if (arweaveConfig.protocol === 'https') {
 } else {
   warp = WarpFactory.forLocal()
 }
-const APP_NAME = process.env.APP_NAME || 'ArtByCity-Development'
-const APP_VERSION = process.env.APP_VERSION || 'development'
+
+warp = warp.use(new DeployPlugin())
 
 export async function deployWarpContract(
   contractPath: string,
   contractName: string,
   wallet: JWKInterface,
   deployOnly: boolean = false,
+  contractVersion: string = '0.0.1',
   initialState?: any
 ) {
   const src = (await fs.readFile(contractPath)).toString()
   const initState = JSON.stringify(initialState)
-  const tags: Tags = [
-    { name: 'Protocol', value: 'ArtByCity' },
-    { name: 'App-Name', value: APP_NAME },
-    { name: 'App-Version', value: APP_VERSION },
-    { name: 'Contract-Name', value: contractName }
+  const tags = [
+    new Tag('Protocol', 'ArtByCity'),
+    new Tag('Contract-Name', contractName),
+    new Tag('Contract-Version', contractVersion)
   ]
 
-  console.log(`Deploying ${contractName} contract`)
+  console.log(`Deploying contract ${contractName} @ ${contractVersion}`)
 
   if (deployOnly) {
-    const srcTx = await warp.createSourceTx({ src }, wallet)
-
+    const source = await warp.createSource({ src }, wallet, true) as Transaction
+    
     for (let i = 0; i < tags.length; i++) {
-      const { name, value } = tags[i]
-      srcTx.addTag(name, value)
+      source.addTag(tags[i].name, tags[i].value)
     }
 
-    // Sign contract tx
-    await arweave.transactions.sign(srcTx, wallet)
+    await arweave.transactions.sign(source, wallet)
+    await warp.saveSource(source, true)
 
-    // Deploy contract tx
-    await arweave.transactions.post(srcTx)
+    console.log(`Deployed contract ${contractName} @ ${contractVersion}`)
+    console.log(`Contract Source TXID: ${source.id}`)
 
-    console.log(`Deployed ${contractName} contract:`)
-    console.log(`\tContract Source TXID: ${srcTx.id}`)
-
-    return { srcTxId: srcTx.id }
+    return { srcTxId: source.id }
   } else {
     const { contractTxId, srcTxId } = await warp.deploy({
       wallet,
@@ -73,78 +72,6 @@ export async function deployWarpContract(
     console.log(`\tInitial State TXID: ${contractTxId}`)
 
     return { contractTxId, srcTxId }
-  }
-}
-
-export async function deployContract(
-  contractName: string,
-  keyfilePath: string,
-  deployOnly: boolean = false
-) {
-  if (!contractName) {
-    throw new Error('No contract specified')
-  }
-
-  if (!keyfilePath) {
-    throw new Error('No deployer keyfile specified')
-  }
-
-  // Read wallet file, path from environment
-  const wallet = JSON.parse((await fs.readFile(keyfilePath)).toString())
-
-  // Read contract source JS file
-  const contractSourceJS = await fs.readFile(
-    `dist/contracts/${contractName}/contract.js`
-  )
-
-  // Read initial state JSON file if initializing
-  let initialStateJSON: Buffer | null = null
-  if (!deployOnly) {
-    initialStateJSON = await fs.readFile(
-      `src/contracts/${contractName}/state.json`
-    )
-  }
-
-  // Create contract tx
-  const contractTx = await arweave.createTransaction(
-    { data: contractSourceJS },
-    wallet
-  )
-  contractTx.addTag('Protocol', 'ArtByCity')
-  contractTx.addTag('App-Name', APP_NAME)
-  contractTx.addTag('App-Version', APP_VERSION)
-  contractTx.addTag('Content-Type', 'application/javascript')
-
-  // Sign contract tx
-  await arweave.transactions.sign(contractTx, wallet)
-  const contractTxId = contractTx.id
-
-  // Deploy contract tx
-  await arweave.transactions.post(contractTx)
-
-  console.log('Contract TX ID', contractTxId)
-
-  if (!deployOnly && initialStateJSON) {
-    // Create initial state tx
-    const initialStateTx = await arweave.createTransaction(
-      { data: initialStateJSON },
-      wallet
-    )
-    initialStateTx.addTag('Protocol', 'ArtByCity')
-    initialStateTx.addTag('App-Name', APP_NAME)
-    initialStateTx.addTag('App-Version', APP_VERSION)
-    initialStateTx.addTag('Content-Type', 'application/json')
-    initialStateTx.addTag('Contract-Src', contractTxId)
-    initialStateTx.addTag('Contract-Name', `${contractName}`)
-
-    // Sign initial state tx
-    await arweave.transactions.sign(initialStateTx, wallet)
-    const initialStateTxId = initialStateTx.id
-
-    // Deploy initial state tx
-    await arweave.transactions.post(initialStateTx)
-
-    console.log('Initial State TX ID', initialStateTxId)
   }
 }
 
@@ -167,9 +94,19 @@ export async function deployContract(
       || args.d
       || args.deploy
       || process.env.DEPLOY_ONLY === 'true'
+    const contractVersion = args._[4]
+      || args.v
+      || args.version
+      || process.env.CONTRACT_VERSION
 
     const wallet = JSON.parse((await fs.readFile(keyfilePath)).toString())
-    await deployWarpContract(contractPath, contractName, wallet, deployOnly)
+    await deployWarpContract(
+      contractPath,
+      contractName,
+      wallet,
+      deployOnly,
+      contractVersion
+    )
   } catch (error) {
     console.error(error)
   }
